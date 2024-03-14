e01989c6cc6674267a84c6c55353145d93cf2d4b
Shawn Kim <tkim1@email.arizona.edu>
2024-03-13 18:08:56 -0700

    comment src/main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from game.wordle_game import WordleGame
 
 def main():
+    # new wordle game object
     wordle_game = WordleGame('game/valid-wordle-words.txt')
+
+    # play as many rounds as player likes
     while True:
         wordle_game.play()
         inp = input('Play another round (y/n)? ')
