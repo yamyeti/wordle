fb87a4c828e45053b6228ccf5e1494265c661d18
Shawn Kim <tkim1@email.arizona.edu>
2024-03-20 11:38:05 -0700

    fix bug with wordle.py, add more directories, add positional_index.py

---
+++ src/data_structures/positional_index.py
@@ -0,0 +1,9 @@
+from game.wordle import *
+
+class PositionalIndex:
+    pos_index = {}
+
+    def __init__(self):
+        self.build_pos_index()
+
+    
--- src/main.py
+++ src/main.py
@@ -2,13 +2,14 @@ from game.wordle_game import WordleGame
 
 def main():
     # new wordle game object
-    wordle_game = WordleGame('game/valid-wordle-words.txt')
+    wordle_game = WordleGame()
 
     # play as many rounds as player likes
     while True:
         wordle_game.play()
         inp = input('Play another round (y/n)? ')
         if inp.lower() != 'y':
+            print('Goodbye.')
             break
         print()
 
