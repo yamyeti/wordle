f82e582ba497e9d470259b87b46b915d72e79f7f
martijnkleiweg <mkleiweg@arizona.edu>
2024-03-23 01:04:57 -0700

    Update 2
    Added some comments

--- src/game/zipfs.py
+++ src/game/zipfs.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 from collections import Counter
 
-# path to wordle-valid-words.txt
+# path to valid-wordle-words.txt
 file_path = 'valid-wordle-words.txt'
 
 # read words
@@ -22,11 +22,17 @@ def starting_letter_frequency(words):
 
 # plot Zipf's law
 def plot_zipfs_law(letter_counts, title):
+
+    # sort letters by frequency
     letters, frequencies = zip(*letter_counts.most_common())
     ranks = range(1, len(frequencies) + 1)
+
+    # apply Zipf's law: f = c / r (where f is frequency, r is rank, c is frequency of most common letter)
     constant = frequencies[0]
     expected_frequencies = [constant / r for r in ranks]
     plt.figure(figsize=(10, 5))
+
+    # plotting actual and expected frequencies (log-log plot)
     plt.loglog(ranks, frequencies, marker='o', linestyle='none', label='Actual Frequencies')
     plt.loglog(ranks, expected_frequencies, label='Expected by Zipf\'s Law', linestyle='--')
     plt.xlabel('Rank of letter (most common to least common)')
